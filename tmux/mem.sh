a5b561685881dcf91096745a28f779a016dfe8ad
Zach Strudler <strudlez@gmail.com>
2014-09-25 19:44:16 -0400

    fix mem

--- tmux/mem.sh
+++ tmux/mem.sh
@@ -4,8 +4,8 @@ if [ $IS_OSX ]; then
   vm_stat 1 | head -n 3 | tail -n 1 | awk '{printf("%d%%", (($1 + $4) / ($1 + $2 + $4) * 100))}'
 else
   free=$(free -t)
-  MEM=$($free | grep Mem | awk '{printf("%d", (($3-$7) / $2) * 100)}')
-  SWAP=$($free | grep Swap | awk '{printf("%d", ($3 / $2) * 100)}')
+  MEM=$(echo $free | grep Mem | awk '{printf("%d", (($3-$7) / $2) * 100)}')
+  SWAP=$(echo $free | grep Swap | awk '{printf("%d", ($3 / $2) * 100)}')
 
   echo "$MEM%|$SWAP%"
 fi
