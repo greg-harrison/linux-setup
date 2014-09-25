d582ea857f7b19b5909523e0e3cab425ea53cf64
Zach Strudler <strudlez@gmail.com>
2014-09-25 19:18:32 -0400

    osx cpu/mem stats

--- tmux/cpu.sh
+++ tmux/cpu.sh
@@ -1,2 +1,6 @@
-#!/usr/bin/env sh
-mpstat 1 | head -n 4 | tail -n 1 | awk '{printf("CPU:%.2f%", 100-$13)}'
+#!/bin/sh
+if [ $IS_OSX ]; then
+  top -l 1 | head -n 10 | grep CPU | grep -Eo '\d*\.\d*% user' | cut -d'%' -f 1
+else
+  mpstat 1 | head -n 4 | tail -n 1 | awk '{printf("%.2f", 100-$13)}'
+fi
--- tmux/mem.sh
+++ tmux/mem.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+if [ $IS_OSX ]; then
+  vm_stat 1 | head -n 3 | tail -n 1 | awk '{printf("%d%%", (($1 + $4) / ($1 + $2 + $4) * 100))}'
+else
+  free=$(free -t)
+  MEM=$($free | grep Mem | awk '{printf("%d", (($3-$7) / $2) * 100)}')
+  SWAP=$($free | grep Swap | awk '{printf("%d", ($3 / $2) * 100)}')
 
-MEM=$(free -t | grep Mem | awk '{printf("%d", (($3-$7) / $2) * 100)}')
-SWAP=$(free -t | grep Swap | awk '{printf("%d", ($3 / $2) * 100)}')
-
-echo "Mem:$MEM%|$SWAP%"
+  echo "$MEM%|$SWAP%"
+fi
