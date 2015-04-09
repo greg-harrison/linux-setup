fb8670b83edfb3ed571ab2409c4f104cd96179a8
Orion Free <orionstein@gmail.com>
2015-04-09 17:04:31 +0000

    More Dependencies

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -6,6 +6,6 @@ sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
 curl -sL https://deb.nodesource.com/setup | sudo bash -
-sudo apt-get install -y ruby zsh git vim nodejs build-essential
+sudo apt-get install -y ruby zsh git vim nodejs npm build-essential cmake python python-dev libpython-all-dev
 sudo chsh -s /usr/bin/zsh $_user
-./install.rb
+./install.rb | zsh -
