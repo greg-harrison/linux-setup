6bf6ea1b2e062916bfc46d9dfa0455bf23645f49
Orion Free <orionstein@gmail.com>
2015-04-09 11:46:25 -0400

    Catching Mango - needing to -y installs

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -2,8 +2,10 @@
 
 _user="$(id -u -n)"
 echo "Installing System, $_user"
-sudo apt-get install ruby-2.0 zsh git vim 
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade -y
 curl -sL https://deb.nodesource.com/setup | sudo bash -
-sudo apt-get install nodejs build-essential
+sudo apt-get install -y ruby zsh git vim nodejs build-essential
 sudo chsh -s /usr/bin/zsh $_user
 ./install.rb
